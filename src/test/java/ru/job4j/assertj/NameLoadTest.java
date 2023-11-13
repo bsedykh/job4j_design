87093259e56ef8fc2f93e3a3c82440d3b81c2d3b
Boris Sedykh <sedykh.boris@yandex.ru>
2023-11-13 19:40:30 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkMissingSeparator() {
+        NameLoad nameLoad = new NameLoad();
+        var input = "keyvalue";
+        assertThatThrownBy(() -> nameLoad.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(input);
+    }
+
+    @Test
+    void checkMissingKey() {
+        NameLoad nameLoad = new NameLoad();
+        var input = "=value";
+        assertThatThrownBy(() -> nameLoad.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(input);
+    }
+
+    @Test
+    void checkMissingValue() {
+        NameLoad nameLoad = new NameLoad();
+        var input = "key=";
+        assertThatThrownBy(() -> nameLoad.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(input);
+    }
+}
