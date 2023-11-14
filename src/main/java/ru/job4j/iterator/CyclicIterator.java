79ab2b742e02cca9772b945c2340d034c598a1ac
Boris Sedykh <sedykh.boris@yandex.ru>
2023-11-14 20:21:55 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,31 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+    private final List<T> data;
+    private Iterator<T> iterator;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+        iterator = data.iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !data.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (!iterator.hasNext()) {
+            iterator = data.iterator();
+        }
+        return iterator.next();
+    }
+}
