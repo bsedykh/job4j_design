2a81ea60f0999ff849b866cc5f36a408bf8b1f36
Boris Sedykh <sedykh.boris@yandex.ru>
2023-11-24 19:12:39 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/MultipleToFile.java
@@ -0,0 +1,16 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultipleToFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/multiple.txt")) {
+            for (int i = 2; i <= 9; i++) {
+                out.write("1 * %d = %d".formatted(i, i).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
