c29345d5c839d891a34caee1b0e8c2126c8ccd6e
Boris Sedykh <sedykh.boris@yandex.ru>
2023-12-01 20:00:04 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,10 +3,14 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
+            Pattern pattern = Pattern.compile("\\?msg=(\\w+)");
             boolean exit = false;
             while (!server.isClosed() && !exit) {
                 Socket socket = server.accept();
@@ -14,13 +18,24 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("?msg=Bye")) {
-                            exit = true;
+                    String line = in.readLine();
+                    Matcher matcher = pattern.matcher(line);
+                    String param = matcher.find() ? matcher.group(1) : null;
+                    exit = "Exit".equals(param);
+                    if (!exit) {
+                        String response;
+                        if ("Hello".equals(param) || "What".equals(param)) {
+                            response = param;
+                        } else {
+                            StringJoiner joiner = new StringJoiner("\r\n");
+                            joiner.add(line);
+                            for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                                joiner.add(str);
+                            }
+                            response = joiner.toString();
                         }
-                        System.out.println(str);
+                        out.write(response.getBytes());
                     }
-                    out.flush();
                 }
             }
         }
