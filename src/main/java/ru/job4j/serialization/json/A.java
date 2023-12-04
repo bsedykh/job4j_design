e1b102260443dc3e7960a68e803c3d23fdc4bbf0
Boris Sedykh <sedykh.boris@yandex.ru>
2023-12-04 17:06:11 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064 #464327]

--- src/main/java/ru/job4j/serialization/json/A.java
+++ src/main/java/ru/job4j/serialization/json/A.java
@@ -1,11 +1,8 @@
 package ru.job4j.serialization.json;
 
-//import org.json.JSONPropertyIgnore;
-
 public class A {
     private B b;
 
-//    @JSONPropertyIgnore
     public B getB() {
         return b;
     }
