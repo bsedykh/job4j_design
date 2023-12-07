fc263e17c636027f4885bbe56ca9d8a2250f1eea
Boris Sedykh <sedykh.boris@yandex.ru>
2023-12-07 21:16:53 +0300

    1. Представления [#504792]

---
+++ db/views.sql
@@ -0,0 +1,58 @@
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+insert into orders (book_id, student_id, active) values (3, 2, false);
+
+create view active_orders as
+select
+    o.id,
+    s.name student,
+    b.name book,
+    a.name author
+from orders o
+    left join books b
+        left join authors a
+            on b.author_id = a.id
+        on o.book_id = b.id
+    left join students s
+        on o.student_id = s.id
+where o.active;
+
+select * from active_orders;
